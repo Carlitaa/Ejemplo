1491dbd5ac4a7d5db9d6289e3ad844a192d6cabc
Administrador <Administrador@SPO_LAB0318.inacap.cl>
2014-04-25 10:03:03 -0300

    Ejemplo1

---
+++ src/ejemplo1/Ejemplo1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ejemplo1;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Ejemplo1 {
+
+   String nombre, apellido, rut, valida_rut;
+
+  
+  
+    public Ejemplo1() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getValida_rut() {
+        return valida_rut;
+    }
+
+    public void setValida_rut(String valida_rut) {
+        this.valida_rut = valida_rut;
+    }
+    
+      
+}
